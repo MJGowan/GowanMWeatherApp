8ab01b7ac24258d9ad1a76a370f933674fe19262
Madeline <madelinejgowan@gmail.com>
2023-01-26 11:35:14 -0800

    1/26/2023 11:35AM

--- scripts/app.js
+++ scripts/app.js
@@ -12,6 +12,18 @@ let dayThreeTemp = document.getElementById("dayThreeTemp");
 let dayFourTemp = document.getElementById("dayFourTemp");
 let dayFiveTemp = document.getElementById("dayFiveTemp");
 
+let dayOneWeather = document.getElementById("dayOneWeather");
+let dayTwoWeather = document.getElementById("dayTwoWeather");
+let dayThreeWeather = document.getElementById("dayThreeWeather");
+let dayFourWeather = document.getElementById("dayFourWeather");
+let dayFiveWeather = document.getElementById("dayFiveWeather");
+
+let dayOneDate = document.getElementById("dayOneDate");
+let dayTwoDate = document.getElementById("dayTwoDate");
+let dayThreeDate = document.getElementById("dayThreeDate");
+let dayFourDate = document.getElementById("dayFourDate");
+let dayFiveDate = document.getElementById("dayFiveDate");
+
 let griffinsApi = "";
 let fiveDayApi = "";
 
@@ -36,18 +48,18 @@ async function GetCurrentWeather() {
     latitude = griffinsApi.coord.lat;
     longitude = griffinsApi.coord.lon;
     currentTemp.innerHTML = griffinsApi.main.temp + " °F";
-    weather.innerHTML = griffinsApi.weather.main;
+    weather.innerHTML = griffinsApi.weather[0].main;
     cityState.innerHTML = search.value;
 
-    minTemp.innerHTML = griffinsApi.main.temp_min;
-    maxTemp.innerHTML = griffinsApi.main.temp_max;
+    minTemp.innerHTML = griffinsApi.main.temp_min + " °F";
+    maxTemp.innerHTML = griffinsApi.main.temp_max + " °F";
 
     console.log("Current Temp: " + griffinsApi.main.temp + " °F");
-    console.log("Min Temp: " + minTemp);
-    console.log("Max Temp: " + maxTemp);
+    console.log("Min Temp: " + minTemp.innerHTML);
+    console.log("Max Temp: " + maxTemp.innerHTML);
     console.log("Lat: " + latitude);
     console.log("Lon: " + longitude);
-    console.log("Current Weather: " + griffinsApi.weather.description);
+    console.log("Current Weather: " + griffinsApi.weather[0].main);
 
     GetFiveDayWeather();
 }
@@ -58,15 +70,28 @@ async function GetFiveDayWeather() {
     const data = await promise.json();
     fiveDayApi = data;
 
-    dayOneTemp.innerHTML = fiveDayApi.list[1].main.temp;
-    dayTwoTemp.innerHTML = fiveDayApi.list[9].main.temp;
-    dayThreeTemp.innerHTML = fiveDayApi.list[17].main.temp;
-    dayFourTemp.innerHTML = fiveDayApi.list[25].main.temp;
-    dayFiveTemp.innerHTML = fiveDayApi.list[33].main.temp;
+    dayOneTemp.innerHTML = fiveDayApi.list[5].main.temp + " °F";
+    dayTwoTemp.innerHTML = fiveDayApi.list[13].main.temp + " °F";
+    dayThreeTemp.innerHTML = fiveDayApi.list[21].main.temp + " °F";
+    dayFourTemp.innerHTML = fiveDayApi.list[29].main.temp + " °F";
+    dayFiveTemp.innerHTML = fiveDayApi.list[37].main.temp + " °F";
 
-    console.log("Tomorrow's Temp: " + dayOneTemp.innerHTML);
-    console.log("Day Two Temp: " + dayTwoTemp.innerHTML);
-    console.log("Day Three Temp: " + dayThreeTemp.innerHTML);
-    console.log("Day Four Temp: " + dayFourTemp.innerHTML);
-    console.log("Day Five Temp: " + dayFiveTemp.innerHTML);
+    dayOneWeather.innerHTML = fiveDayApi.list[5].weather[0].main;
+    dayTwoWeather.innerHTML = fiveDayApi.list[13].weather[0].main;
+    dayThreeWeather.innerHTML = fiveDayApi.list[21].weather[0].main;
+    dayFourWeather.innerHTML = fiveDayApi.list[29].weather[0].main;
+    dayFiveWeather.innerHTML = fiveDayApi.list[37].weather[0].main;
+
+    dayOneDate.innerHTML = fiveDayApi.list[5].dt_txt;
+    dayTwoDate.innerHTML = fiveDayApi.list[13].dt_txt;
+    dayThreeDate.innerHTML = fiveDayApi.list[21].dt_txt;
+    dayFourDate.innerHTML = fiveDayApi.list[29].dt_txt;
+    dayFiveDate.innerHTML = fiveDayApi.list[27].dt_txt;
+
+    console.log("Tomorrow: " + dayOneTemp.innerHTML + ", " + dayOneWeather.innerHTML);
+    console.log("Day Two: " + dayTwoTemp.innerHTML + ", " + dayTwoWeather.innerHTML);
+    console.log("Day Three: " + dayThreeTemp.innerHTML + ", " + dayThreeWeather.innerHTML);
+    console.log("Day Four: " + dayFourTemp.innerHTML + ", " + dayFourWeather.innerHTML);
+    console.log("Day Five: " + dayFiveTemp.innerHTML + ", " + dayFiveWeather.innerHTML);
+
 }
